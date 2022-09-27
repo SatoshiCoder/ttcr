b85fec423ca46fbc3cf1675c00eb9b7594d1b686
SatoshiCoder <100981673+SatoshiCoder@users.noreply.github.com>
2022-09-27 10:50:14 +0700

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,4 +1,12 @@
-for i in range(100000):
+for i in range(20000):
     print ("Tao thích mày❤")
 
-print('Mày có đòng ý làm ny t ko?')        
+print('Mày có đồng ý làm ny tao không?')
+cr = input('Có hay Không:')
+
+if cr == 'Có':
+    print('Tao cũng thích mày!')
+if cr == 'Không':
+    print('Huhuhu buồn quá:(')        
+else:
+    print("Bạn sai rồi phải viết chữ cái đầu thì mới đúng hoặc bạn viết là: Không như này mới đúng!")     
